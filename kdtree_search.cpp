658afcde005f5ea3412733e14e7529ba54b97296
Shivam Naik <shivamsnaik@gmail.com>
2016-08-11 19:37:23 +0530

    ADDING THE FEATURE OF MATCHING THE K-NEIGHBOUR POINTS OF SOURCE AND DESTINATION

--- kdtree_search.cpp
+++ kdtree_search.cpp
@@ -28,17 +28,20 @@ main (int argc, char** argv)
     cloud->points[i].z = 1024.0f * rand () / (RAND_MAX + 1.0f);
   }
 
+  // CREATING THE KDTEE OBJECT
   KdTreeFLANN<PointXYZ> kdtree;
 
+  // GIVING THE POINT CLOUD AS THE INPUT FOR FORMING THE TREE
   kdtree.setInputCloud (cloud);
 
+  //CHOOSING A SOURCE POINT FROM THE POINT CLOUD
   PointXYZ source;
 
   source.x = 1024.0f * rand () / (RAND_MAX + 1.0f);
   source.y = 1024.0f * rand () / (RAND_MAX + 1.0f);
   source.z = 1024.0f * rand () / (RAND_MAX + 1.0f);
 
-	
+  //CHOOSING A DESTINATION POINT FROM THE POINT CLOUD
   PointXYZ destin;
 
   destin.x = 1024.0f * rand () / (RAND_MAX + 1.0f);
@@ -47,7 +50,7 @@ main (int argc, char** argv)
 
 
 
-  // K nearest neighbor search
+  // K NEAREST SEARCH FOR BOTH SOURCE AND THE DESTINATION POINT UNTIL THERE IS MATCH B/W THE K POINTS OF SOURCE AND THE DESTINATION
 
   int K = 10;
 
@@ -56,10 +59,27 @@ main (int argc, char** argv)
 
   vector<int> destNPts(K);
   vector<float> destNPtsSqUnits(K);
-
+  int count;
+  
   while(1)
  {
-
+	kdtree.nearestKSearch(source,K,sourceNPts,sourceNPtsSqUnits);
+	kdtree.nearestKSearch(destin,K,destNPts,destNPtsSqUnits);
+	count = 0;
+	
+	for(int i=0; (i < sourceNPts.size()) ; i++)
+     	{
+		for(int j=0;j < (destNPts.size());j++)
+		{
+			if(sourceNPts[i] == destNPts[j])
+				count++;
+		}
+	}
+	
+	if(count <=5)
+		cout<<"THERE WAS NO MATCH OF POINTS FROM SOURCE TO DESTIN"<<endl;
+	else
+		break;
  }
 
   return 0;
